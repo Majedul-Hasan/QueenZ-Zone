417cdbc300b35599621e1e131c2dcfd9d31db6eb
Roni Ahmed <rony13637@gmail.com>
2022-03-27 17:15:15 +0300

    Initial commit

--- src/Components/MyAccount/MyAccount.js
+++ src/Components/MyAccount/MyAccount.js
@@ -1,9 +1,39 @@
+import { faG } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+} from "../FirebaseAuth/FirebaseAuth";
 
 export default function MyAccount() {
   // register btn
   const registerBtn = () => {
     console.log("clicked");
+  };
+  // LogInwithGoogle
+  const LogInwithGoogle = () => {
+    console.log("google");
+    const provider = new GoogleAuthProvider();
+    const auth = getAuth();
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        const user = result.user;
+        console.log(user, token);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+
+        const email = error.email;
+
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        console.log(error);
+      });
   };
 
   return (
@@ -136,6 +166,42 @@ export default function MyAccount() {
               REGISTER
             </span>
           </span>
+        </div>
+        <div class="d-flex justify-content-center mt-3">
+          <span
+            pan
+            style={{
+              fontSize: "14px",
+              fontFamily: "Poppins",
+              fontWeight: "400",
+              opacity: "0.5",
+            }}
+          >
+            -----Or Join With-----
+          </span>
+        </div>
+        <div
+          class="d-flex mt-4 justify-content-center "
+          onClick={() => LogInwithGoogle()}
+        >
+          <div
+            class="d-flex bd-highlight"
+            style={{
+              backgroundColor: "green",
+              color: "white",
+              padding: "5px 30px",
+              borderRadius: "10px",
+              fontSize: "16px",
+              fontFamily: "Poppins",
+              fontWeight: "600",
+              boxShadow: "rgb(213 205 149)  0 3px 7px",
+            }}
+          >
+            <div class="p-2 w-100 bd-highlight">Google</div>
+            <div class="p-2 flex-shrink-1 bd-highlight">
+              <FontAwesomeIcon icon={faG} />
+            </div>
+          </div>
         </div>
       </div>
     </div>
