a3aad4ec718a1142748cfba749659be893636355
Roni Ahmed <rony13637@gmail.com>
2022-04-04 01:30:06 +0300

    Initial committt

--- src/Components/Dashboard/Functionbar/Functionbar.js
+++ src/Components/Dashboard/Functionbar/Functionbar.js
@@ -1,5 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Functionbar() {
-  return <div>lorem1000</div>;
+  const [imageNumbr, setImageNumber] = useState([]);
+
+  const addImage = (props) => {
+    console.log("dkj");
+    setImageNumber([...imageNumbr, props]);
+  };
+
+  const seeeeee = () => {
+    console.log("this is :::::        ", imageNumbr);
+  };
+
+  return (
+    <div>
+      <button onClick={() => addImage("1")}>Add</button>
+      <button onClick={() => seeeeee()}>seeeeeee</button>
+      {imageNumbr.map((dt) => (
+        <h2>hiiiiii</h2>
+      ))}
+    </div>
+  );
 }
